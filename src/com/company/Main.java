30ed7f6cfc2e638ad929b2514ae8366f096c9239
Fleetlord1 <Fleetlord1@users.noreply.github.com>
2021-09-07 10:31:13 +0300

    Privet

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,39 @@
 package com.company;
 
+import java.sql.SQLOutput;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int[] a ={10,2,13,51,123};
+
+        int[] b = a;
+
+        b[3] =12;
+
+
+        for(int i =0;i<a.length;i++) {
+            b[i]++;
+            System.out.print(b[i]+ "\t");
+        }
+        System.out.println();
+
+        for(int var : a){
+            var++;
+            System.out.print(var + "\t");
+        }
+
+        System.out.println();
+
+        String[] str = {"dwan","dadaw","dwada","swdas"};
+
+        for(String var : str){
+            var= var+"sssss";
+            System.out.print(var + "\t");
+
+        }
+
+
+
     }
 }
